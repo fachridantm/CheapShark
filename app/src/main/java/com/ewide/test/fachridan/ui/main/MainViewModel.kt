f70dfc36ba37a5e9e0c81b61f34f3ff9b46bc41a
fachridantm <fachridantm@gmail.com>
2023-07-11 16:46:22 +0700

    create MainViewModel

---
+++ app/src/main/java/com/ewide/test/fachridan/ui/main/MainViewModel.kt
@@ -0,0 +1,14 @@
+package com.ewide.test.fachridan.ui.main
+
+import androidx.lifecycle.ViewModel
+import androidx.lifecycle.asLiveData
+import com.ewide.test.fachridan.core.domain.usecase.DealsUseCase
+import dagger.hilt.android.lifecycle.HiltViewModel
+import javax.inject.Inject
+
+@HiltViewModel
+class MainViewModel @Inject constructor(
+    private val dealsUseCase: DealsUseCase
+) : ViewModel() {
+    fun getListOfDeals() = dealsUseCase.getListOfDeals().asLiveData()
+}
