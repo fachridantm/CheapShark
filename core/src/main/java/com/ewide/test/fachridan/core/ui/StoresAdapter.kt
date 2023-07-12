3b634144c1eb32d7683c3b4b4fd3cb83677d6446
fachridantm <fachridantm@gmail.com>
2023-07-12 23:54:36 +0700

    create ui for list store on GameDetailsActivity

--- core/src/main/java/com/ewide/test/fachridan/core/ui/StoresAdapter.kt
+++ core/src/main/java/com/ewide/test/fachridan/core/ui/StoresAdapter.kt
@@ -2,29 +2,30 @@ package com.ewide.test.fachridan.core.ui
 
 import android.view.LayoutInflater
 import android.view.ViewGroup
-import androidx.paging.PagingDataAdapter
 import androidx.recyclerview.widget.DiffUtil
+import androidx.recyclerview.widget.ListAdapter
 import androidx.recyclerview.widget.RecyclerView
 import com.ewide.test.fachridan.core.databinding.ItemListStoreBinding
 import com.ewide.test.fachridan.core.domain.model.Store
+import com.ewide.test.fachridan.core.utils.combineWithHost
 import com.ewide.test.fachridan.core.utils.convertToPrice
 import com.ewide.test.fachridan.core.utils.loadImage
 import com.ewide.test.fachridan.core.utils.showStrikeThrough
 
 class StoresAdapter(private val onItemClick: (Store) -> Unit) :
-    PagingDataAdapter<Store, StoresAdapter.ViewHolder>(DIFF_CALLBACK) {
+    ListAdapter<Store, StoresAdapter.ViewHolder>(DIFF_CALLBACK) {
 
     inner class ViewHolder(private val binding: ItemListStoreBinding) :
         RecyclerView.ViewHolder(binding.root) {
         fun bind(store: Store) {
             with(binding) {
-                itemStoreTvTitle.text = store.storeName
                 itemStoreTvNormalPrice.apply {
                     text = store.normalPrice.convertToPrice()
                     showStrikeThrough()
                 }
                 itemStoreTvSalePrice.text = store.salePrice.convertToPrice()
-                itemStoreIvLogo.loadImage(store.storeLogo)
+                itemStoreIvBanner.loadImage(store.storeLogo.combineWithHost())
+                itemView.setOnClickListener { onItemClick(store) }
             }
         }
     }
