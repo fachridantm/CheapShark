e4f26061ad3120ea35f858b5a1a6b671148a09d2
fachridantm <fachridantm@gmail.com>
2023-07-12 17:50:24 +0700

    remove null value

--- core/src/main/java/com/ewide/test/fachridan/core/data/source/remote/response/DealsItem.kt
+++ core/src/main/java/com/ewide/test/fachridan/core/data/source/remote/response/DealsItem.kt
@@ -4,18 +4,18 @@ import com.google.gson.annotations.SerializedName
 
 data class DealsItem(
 
-	@field:SerializedName("dealID")
-	val dealID: String,
+    @field:SerializedName("dealID")
+    val dealID: String,
 
-	@field:SerializedName("price")
-	val price: String? = null,
+    @field:SerializedName("price")
+    val price: String? = null,
 
-	@field:SerializedName("savings")
-	val savings: String? = null,
+    @field:SerializedName("savings")
+    val savings: String? = null,
 
-	@field:SerializedName("storeID")
-	val storeID: String,
+    @field:SerializedName("storeID")
+    val storeID: String,
 
-	@field:SerializedName("retailPrice")
-	val retailPrice: String? = null
+    @field:SerializedName("retailPrice")
+    val retailPrice: String? = null
 )
