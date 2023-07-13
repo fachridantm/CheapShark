05f19035075b28345f8a77f007a9c2d027f49d04
fachridantm <fachridantm@gmail.com>
2023-07-13 15:43:13 +0700

    create Constants for sorting

--- core/src/main/java/com/ewide/test/fachridan/core/utils/Constants.kt
+++ core/src/main/java/com/ewide/test/fachridan/core/utils/Constants.kt
@@ -2,4 +2,7 @@ package com.ewide.test.fachridan.core.utils
 
 object Constants {
     const val EXTRA_DATA = "extra_data"
+    const val DEFAULT = "Deal Rating"
+    const val TITLE = "Title"
+    const val PRICE = "Price"
 }
