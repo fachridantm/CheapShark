0982c6650ccc969edffc8f797ec903e5db447212
fachridantm <fachridantm@gmail.com>
2023-07-13 16:57:55 +0700

    reformat code

--- core/src/main/java/com/ewide/test/fachridan/core/di/NetworkModule.kt
+++ core/src/main/java/com/ewide/test/fachridan/core/di/NetworkModule.kt
@@ -13,7 +13,6 @@ import retrofit2.Retrofit
 import retrofit2.converter.gson.GsonConverterFactory
 import java.util.concurrent.TimeUnit
 
-
 @Module
 @InstallIn(SingletonComponent::class)
 class NetworkModule {
